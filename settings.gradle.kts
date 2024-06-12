7c58a0cddfa4687e5892b5a6b6f1e3587d7fed1f
Pranit Rane <pranitrane619@gmail.com>
2024-06-12 23:41:32 +0530

    first commit

---
+++ settings.gradle.kts
@@ -0,0 +1,19 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+rootProject.name = "NotKraken"
+include(":composeApp")
